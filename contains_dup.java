ed97695c959b78a16fdf68c6d2d5bea9fe24d700
Sudarshan R <radussudarshan@gmail.com>
2025-04-26 18:49:08 +0530

    Create contains_dup.java

---
+++ contains_dup.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/contains-duplicate/description/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]==nums[i]){
+                return true;
+            }    
+        }
+        return false;
+    }
+}
